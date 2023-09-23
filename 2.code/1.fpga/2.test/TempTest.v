229c6d7ef88792ef4f81269be1f4785ff6a55447
Nitin Dogra <90884243+NitinDogra007@users.noreply.github.com>
2023-09-23 14:37:35 +1000

    Updated code with UART communication

--- 2.code/1.fpga/2.test/TempTest.v
+++ 2.code/1.fpga/2.test/TempTest.v
@@ -4,8 +4,10 @@ module TempTest (
   inout singer_bus,
   output [39:0] dataout,
   output reg tick_done,
-  output reg tx_data, // UART Transmit Data
-  output reg tx_ready // UART Transmit Ready
+  output uart_tx, // UART transmit data output
+  output reg uart_tx_en, // UART transmit enable output
+  inout uart_tx_pin, // UART transmit pin
+  inout uart_rx_pin // UART receive pin
 );
 
   parameter CLK_PERIOD_NS = 83; // 12MHz
@@ -22,11 +24,36 @@ module TempTest (
   reg [2:0] state, next_state;
   reg [31:0] index, next_index;
   reg [39:0] data_out, next_data_out;
-  reg [5:0]bit_in, next_bit_in;
+  reg [5:0] bit_in, next_bit_in;
   reg [5:0] number_bit, next_number_bit;
   reg oe;
+  reg uart_tx_data; // UART transmit data
+  reg [3:0] uart_tx_count; // UART transmit count
+  reg rx;        // Current state of the receive line
+  reg next_rx;   // Next state of the receive line
+  wire trigger_condition;
+
+  assign trigger_condition = (rx == 1'b0) && (next_rx == 1'b1);
 
   assign dataout = data_out;
+
+  // UART parameters (adjust as needed)
+  parameter BAUD_RATE = 9600;
+  parameter DATA_BITS = 8;
+  parameter STOP_BITS = 1; // Define the number of stop bits here
+  reg [15:0] baud_divisor;
+
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      baud_divisor <= 16'b0;
+    end else begin
+      // Calculate the baud rate divisor
+      baud_divisor <= (16'h8000 / BAUD_RATE); // Assuming a 16-bit divisor
+    end
+  end
+
+  // UART state machine states
+  reg [2:0] uart_state;
 
   // Register
   always @(posedge clk or posedge rst) begin
@@ -42,21 +69,12 @@ module TempTest (
       number_bit <= next_number_bit;
       bit_in <= next_bit_in;
       data_out <= next_data_out;
+
+      // UART state machine
+      uart_tx_en <= (uart_state != 0) ? 1'b1 : 1'b0;
     end
   end
-  
-    // UART Transmit Registers
-  reg [7:0] tx_data_reg;  // Data to be transmitted
-  reg [2:0] tx_state;     // UART Transmit State Machine
-  reg [3:0] tx_counter;   // UART Transmit Counter
 
-  // Initialize UART Transmit Registers
-  initial begin
-    tx_data_reg = 8'b0;
-    tx_state = 3'b000;
-    tx_counter = 4'b0000;
-  end
-	
   // State machine process
   always @(posedge clk) begin
     tick_done <= 1'b0;
@@ -68,7 +86,7 @@ module TempTest (
     oe = 1'b0;
     next_bit_in = bit_in;
 
-    case(state)
+    case (state)
       0: begin // reset
         if (index == 0) begin
           next_state = 1;
@@ -99,8 +117,8 @@ module TempTest (
           next_state = 2;
         end
       end
-		
-		3: begin // response_sl
+	  
+	  3: begin // response_sl
         if (bit_in == 1'b0 && next_bit_in == 1'b1) begin
           next_state = 4;
           next_index = DELAY_80_US; // Use DELAY_80_US here
@@ -150,7 +168,7 @@ module TempTest (
 
       default: begin
         // Handle undefined states here, if necessary
-		  next_state = 0; // Set to the reset state
+        next_state = 0; // Set to the reset state
         next_index = MAX_DELAY; // Set other default values if needed
         next_number_bit = 0; // Set other default values if needed
         next_bit_in = 1'b1; // Set other default values if needed
@@ -158,30 +176,52 @@ module TempTest (
       end
     endcase
   end
-  
-	// UART Transmit State Machine
-	always @(posedge clk) begin
-	  case (tx_state)
-		 3'b000: begin // IDLE state
-			if (tx_counter > 8) begin
-			  tx_state <= 3'b001; // Move to the TRANSMITTING state
-			  tx_data_reg <= dataout; // Load temperature data
-			end
-		 end
-		 3'b001: begin // TRANSMITTING state
-			if (tx_counter < 8) begin
-			  tx_data <= tx_data_reg[tx_counter]; // Transmit one bit at a time
-			  tx_ready <= 1'b1; // Set UART ready
-			  tx_counter <= tx_counter + 1; // Increment counter
-			end else begin
-			  tx_ready <= 1'b0; // Clear UART ready when all bits are transmitted
-			  tx_state <= 3'b000; // Return to IDLE state
-			end
-		 end
-	  endcase
-	end
 
-	// Logic for singer_bus
+  // UART TX pin assignment
+  assign uart_tx_pin = uart_tx_en ? uart_tx_data : 1'bz;
+
+  // UART state machine
+  always @(posedge clk) begin
+    case (uart_state)
+      0: begin // UART state 0 - Idle
+        if (trigger_condition) begin
+          // Transition to the next UART state to start transmitting
+          uart_state <= 1; // Change to the appropriate state
+          uart_tx_data <= 8'hFF; // Set UART transmit data (adjust as needed)
+          uart_tx_count <= 4'b0000; // Initialize transmit count (adjust as needed)
+        end else begin
+          uart_state <= 0; // Stay in the idle state
+        end
+      end
+
+      1: begin // UART state 1 - Transmitting
+        if (uart_tx_count < DATA_BITS) begin
+          // Continue transmitting data bits
+          uart_tx_data <= data_out[uart_tx_count]; // Assuming data_out is a 40-bit signal
+          uart_tx_count <= uart_tx_count + 1'b1;
+        end else begin
+          // Finished transmitting all data bits, move to the stop bit(s) state
+          uart_state <= 2; // Change to the stop bit(s) state
+          uart_tx_data <= 1'b0; // Set stop bit(s) value (adjust as needed)
+          uart_tx_count <= 4'b0000; // Reset transmit count for stop bit(s)
+        end
+      end
+
+      2: begin // UART state 2 - Stop bit(s)
+        if (uart_tx_count < STOP_BITS) begin
+          // Continue transmitting stop bit(s)
+          uart_tx_data <= 1'b0; // Set stop bit(s) value (adjust as needed)
+          uart_tx_count <= uart_tx_count + 1'b1;
+        end else begin
+          // Finished transmitting stop bit(s), move to idle state
+          uart_state <= 0; // Change to the idle state
+          // Optionally, you can reset other UART-related variables here
+        end
+      end
+    endcase
+  end
+
+  // Logic for singer_bus
   assign singer_bus = (oe) ? 1'b0 : 1'bz;
 
 endmodule
